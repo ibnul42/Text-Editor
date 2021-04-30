f8ede60d270d6606686dc48b2718920bfa70c77e
ibnul42 <ibnulashir42@gmail.com>
2021-04-30 09:31:42 +0600

    scripts turned into ES6

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-function updateText() {
+updateText = () => {
     let text = document.getElementById("text-input").value;
     document.getElementById("text-output").innerText = text;
 }
 
-function makeBold(elem) {
+makeBold = (elem) => {
     elem.classList.toggle('active');
     document.getElementById("text-output").classList.toggle('bold');
 }
 
-function makeItalic(elem) {
+makeItalic = (elem) => {
     elem.classList.toggle('active');
     document.getElementById("text-output").classList.toggle('italic');
 }
-
-function makeUnderline(elem) {    
+makeUnderline = (elem) => {    
     elem.classList.toggle('active'); 
     document.getElementById("text-output").classList.toggle('underline');
 }
 
-function alignText(elem, alignType) {
+alignText = (elem, alignType) => {
     document.getElementById("text-output").style.textAlign = alignType;
     let buttonList = document.getElementsByClassName('align');
-    for (let i=0; i<buttonList.length; i++){
-        buttonList[i].classList.remove('active');
+    for (let key of buttonList) {
+        key.classList.remove('active'); 
     }
+          
+    // for (let i=0; i<buttonList.length; i++){
+    // }
     elem.classList.add('active');
 }
